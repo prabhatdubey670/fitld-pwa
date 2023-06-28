1d333f061ddce6e971897a0cc438d31f34cabea5
prabhat dubey <dprabhat670@gmail.com>
2023-06-28 13:52:12 +0530

    🚀 chore(package.json): add dependencies for Chakra UI, Emotion, and Framer Motion ✨ feat(CTA/page.tsx): create CTA component for landing page ✨ feat(layout.tsx): create LandingpageLayout component ✨ feat(page.tsx): create LandingPage component ✨ feat(provider.tsx): create Providers component for Chakra UI and Emotion

---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CTA from './CTA/page';
+
+export default function LandingPage() {
+  return (
+    <div>
+      <CTA />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
+import LandingPage from './landingpage/page';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <LandingPage />
+    </main>
+  );
 }
